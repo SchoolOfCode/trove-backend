56e2accc0e98c6ed080fc7c56e4f5d89d30a01a1
kianeblackman <kianeblackman@gmail.com>
2022-11-23 16:17:46 +0000

    getPostByTag query and router complete and working

--- Models/posts.js
+++ Models/posts.js
@@ -1,28 +1,58 @@
-const { query } = require("../db/index.js");
+const { query } = require('../db/index.js');
 
 async function getAllPosts() {
-    const results = await query('SELECT * FROM posts');
-    const linksObj = results.rows;
-    return linksObj;
-};
-
-async function addNewPost (post) {
-    const update = await query('INSERT INTO posts (author, title, thumbnail, summary, date_posted, url, tags) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;', 
-    [post.author, post.title, post.thumbnail, post.summary, post.date_posted, post.url, post.tags]);
-    const newPostID = update.rows[0].post_id
-    const newTags = await Promise.all(post.tags.map(async (tag) => { let update =
-        await query('INSERT INTO tags_table (tags, post_id) VALUES ($1, $2) RETURNING *', [tag, newPostID])
-        return update.rows[0];
-    }))
-    const addPost = [update.rows[0], newTags];
-    return addPost;
+  const results = await query('SELECT * FROM posts');
+  const linksObj = results.rows;
+  return linksObj;
 }
 
-async function deletePostByID (id) {
-    const deletedTags = await query('DELETE FROM tags_table WHERE post_id = $1 RETURNING *;', [`${id}`]);
-    const deletedPost = await query('DELETE FROM posts WHERE post_id = $1 RETURNING *;', [`${id}`]);
-    const deleteConfirm = [deletedPost, deletedTags];
-    return deleteConfirm; 
+async function getPostByTag(tag) {
+  const results = await query(
+    'SELECT * FROM posts LEFT JOIN tags_table ON posts.post_id=tags_table.post_id WHERE tags_table.tags=$1;',
+    [tag]
+  );
+  const postArr = results.rows;
+  return postArr;
 }
 
-module.exports = {getAllPosts, addNewPost, deletePostByID}
+async function addNewPost(post) {
+  const update = await query(
+    'INSERT INTO posts (author, title, thumbnail, summary, date_posted, url, tags) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;',
+    [
+      post.author,
+      post.title,
+      post.thumbnail,
+      post.summary,
+      post.date_posted,
+      post.url,
+      post.tags,
+    ]
+  );
+  const newPostID = update.rows[0].post_id;
+  const newTags = await Promise.all(
+    post.tags.map(async (tag) => {
+      let update = await query(
+        'INSERT INTO tags_table (tags, post_id) VALUES ($1, $2) RETURNING *',
+        [tag, newPostID]
+      );
+      return update.rows[0];
+    })
+  );
+  const addPost = [update.rows[0], newTags];
+  return addPost;
+}
+
+async function deletePostByID(id) {
+  const deletedTags = await query(
+    'DELETE FROM tags_table WHERE post_id = $1 RETURNING *;',
+    [`${id}`]
+  );
+  const deletedPost = await query(
+    'DELETE FROM posts WHERE post_id = $1 RETURNING *;',
+    [`${id}`]
+  );
+  const deleteConfirm = [deletedPost, deletedTags];
+  return deleteConfirm;
+}
+
+module.exports = { getAllPosts, getPostByTag, addNewPost, deletePostByID };
--- Routes/posts.js
+++ Routes/posts.js
@@ -1,23 +1,38 @@
-const express = require ("express");
+const express = require('express');
 const router = express.Router();
-const { getAllPosts, addNewPost, deletePostByID } = require ("../Models/posts");
+const {
+  getAllPosts,
+  getPostByTag,
+  addNewPost,
+  deletePostByID,
+} = require('../Models/posts');
 
-router.get("/", async function (req, res) {
-    const postsArr = await getAllPosts();
-    res.json({success:true, payload: postsArr});
-    console.log(res.body)});
+// router.get('/', async function (req, res) {
+//   const postsArr = await getAllPosts();
+//   res.json({ success: true, payload: postsArr });
+//   console.log(res.body);
+// });
 
+router.get('/', async function (req, res) {
+  console.log(req.query);
+  if (req.query.search !== undefined) {
+    const posts = await getPostByTag(req.query.search);
+    res.json({ success: true, playload: posts });
+  }
+  const posts = await getAllPosts();
+  res.json({ success: true, playload: posts });
+  console.log(res.body);
+});
 
-router.post("/", async function (req, res) {
-    const newPostObj = await addNewPost(req.body);
-    res.json({success:true, payload: newPostObj});
-})
+router.post('/', async function (req, res) {
+  const newPostObj = await addNewPost(req.body);
+  res.json({ success: true, payload: newPostObj });
+});
 
-router.delete("/:id", async function (req, res) {
-    const deletedItems = await deletePostByID(req.params.id);
-    res.json({success: true, payload: deletedItems});
-    console.log(res.body)
-})
-
+router.delete('/:id', async function (req, res) {
+  const deletedItems = await deletePostByID(req.params.id);
+  res.json({ success: true, payload: deletedItems });
+  console.log(res.body);
+});
 
 module.exports = router;
