a772bc20d8d4cf39837d735e6c43713e06f48922
ryandonaghue <ryandonaghue@outlook.com>
2022-11-30 15:18:27 +0000

    Added Jsdocs to routes

--- Routes/posts.js
+++ Routes/posts.js
@@ -8,32 +8,33 @@ import {
   deletePostByID,
 } from '../Models/posts.js';
 
-/**
- * 
- */
-//GET route checks for search query and runs getPostByTag
+/** 
+ * GET route checks for search query and runs getPostByTag
+ * if no search query is defined gets all posts
+*/
 router.get('/', async function (req, res) {
-  console.log(req.query);
   if (req.query.search !== undefined) {
     const posts = await getPostByTag(req.query.search);
     res.json({ success: true, payload: posts });
   } else {
   const posts = await getAllPosts();
   res.json({ success: true, payload: posts });
-  console.log(res.body);
 }});
 
-// Routes to the new post function
+/**
+ * Routes to the new post function
+ */
 router.post('/', async function (req, res) {
   const newPostObj = await addNewPost(req.body);
   res.json({ success: true, payload: newPostObj });
 });
 
-// Deletes a post by ID. Not yet hooked up to front end.
+/**
+ * Deletes a post by ID. Not yet hooked up to front end.
+ */
 router.delete('/:id', async function (req, res) {
   const deletedItems = await deletePostByID(req.params.id);
-  res.json({ success: true, payload: deletedItems });
-  console.log(res.body);
+  res.json({ success: true, payload: deletedItems })
 });
 
 export default router;
